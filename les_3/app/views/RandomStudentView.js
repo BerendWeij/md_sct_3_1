25895ea584e3b23dc513216eea0db2b8a52d2d52
Berend Weij <info@berendweij.nl>
2016-02-22 14:59:06 +0100

    added first template example

--- les_3/app/views/RandomStudentView.js
+++ les_3/app/views/RandomStudentView.js
@@ -4,3 +4,33 @@ var app = app || {};
 // dit object is je koppeling met je html om random studenten te kunnen laten zien
 // als je op shuffle klikt, dan moet dit script dit afvangen en actie ondernemen
 // om bij de model iets op te halen
+
+app.randomStudentsView = {
+
+    init: function(){
+
+        // Grab the template script from the dom
+        var templateSrc = document.querySelector("#students-template").innerHTML;
+
+        var testData = {
+            students: [
+                {firstName:"Henk"},
+                {firstName:"Jan"},
+                {firstName:"Piet"}
+            ]
+        };
+
+        // Transform the HTML template into a 'real' template
+        this.template = Handlebars.compile(templateSrc);
+
+        // call the render function
+        this.render(testData);
+
+    },
+
+    render: function(data){
+        // we retrieve the container and fill the HTML with the template + data
+        document.querySelector(".container").innerHTML = this.template(data);
+    }
+
+}
